b89c8ba1dc56416fc4096ac2f3ea4fd67b2b8f15
Alex <zthh626@gmail.com>
2022-03-04 14:31:31 -0800

    feat: integrate ground station

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
+interface Point {
+  lat: number;
+  long: number;
+}
+
 const PORT = 8080;
 
 const app = express();
@@ -15,13 +20,39 @@ app.get("/", (req, res) => {
 
 const usvNamespace = io.of("/usv");
 const groundstationNamespace = io.of("/groundstation");
+let currentRoute: Point[] = [];
 
 usvNamespace.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected to usv");
 });
 
 groundstationNamespace.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected to groundstation");
+  socket.emit("init_route", currentRoute);
+
+  socket.on("add_point", (point: Point) => {
+    currentRoute.push(point);
+    groundstationNamespace.emit("add_point_ack", point);
+    usvNamespace.emit("add_point_ack", point);
+  });
+
+  socket.on("delete_point", (point: Point) => {
+    const newRoute = [...currentRoute];
+    for (let i = 0; i < newRoute.length; i++) {
+      if (newRoute[i].lat === point.lat && newRoute[i].long === point.long) {
+        newRoute.splice(i, 1);
+      }
+    }
+    currentRoute = newRoute;
+    groundstationNamespace.emit("delete_point_ack", currentRoute);
+    usvNamespace.emit("delete_point_ack", currentRoute);
+  });
+
+  socket.on("clear_route", () => {
+    currentRoute = [];
+    groundstationNamespace.emit("clear_route_ack");
+    usvNamespace.emit("clear_route_ack");
+  });
 });
 
 setInterval(() => {
